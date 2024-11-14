e7289367dfa2fba71dcce7ed9cecdededafa38da
Pablo <91567040+pablogrp@users.noreply.github.com>
2024-11-14 10:45:44 +0000

    Add files via upload

---
+++ my-app/target/reports/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"es.ull.app","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"es.ull.app","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
